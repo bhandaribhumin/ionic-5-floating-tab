e7ce289af20e501d7102c313ae35f841c1097e13
Bhumin Bhandari <bhandaribhumin@gmail.com>
2021-06-28 19:33:49 +0530

    Init setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tab-bar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
